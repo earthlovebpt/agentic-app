230fa01874c3c1c2fa8c6ef7d23442742821dbf9
earthlovebpt <earthlovebpt@gmail.com>
2025-04-24 21:34:45 +0700

    first commit

---
+++ agents/schema/bd_state.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+from operator import add
+
+
+class BDState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+    data_insights: Annotated[list, add]
+    search_insights: Annotated[list, add]
