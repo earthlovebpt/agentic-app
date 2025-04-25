be5c7e597096dcc0c6ce525a74eed452122d586d
Supphaset Engphaiboon <supphaset555@gmail.com>
2025-04-25 16:26:03 +0700

    init agent

---
+++ agents/da_agent/graph/state.py
@@ -0,0 +1,14 @@
+import operator
+from typing import Sequence, TypedDict, Annotated, List
+from langchain_core.messages import BaseMessage
+from pydantic import Field
+from typing import Dict, Any
+
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    datasets: Dict[str, Any] = Field(default_factory=dict)  
+    schema_context: str
+    intermediate_outputs: Annotated[List[dict], operator.add]
+    current_variables: dict
+    output_image_paths: Annotated[List[str], operator.add]
